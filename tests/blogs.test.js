08a5953e172a1957bf5de152fad68c07524ca5ca
Dmitry Bondarenko <bondarenko.dmitry.85@gmail.com>
2018-11-26 09:53:03 +0200

    testing done

---
+++ tests/blogs.test.js
@@ -0,0 +1,73 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a[href="/blogs/new"]');
+  });
+
+  test('We can see create-blog form', async () => {
+    const formLabel = await page.getContentsOf('.title label');
+    expect(formLabel).toEqual('Blog Title');
+  });
+
+  describe('When using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My title');
+      await page.type('.content input', 'My content');
+      await page.click('button[type="submit"]');
+    });
+
+    test('Submitting takes user to review page', async () => {
+      const text = await page.getContentsOf('form h5');
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('Submitting takes user to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+      const title = await page.getContentsOf('.card .card-title');
+      const content = await page.getContentsOf('.card p');
+
+      expect(title).toEqual('My title');
+      expect(content).toEqual('My content');
+    });
+  });
+
+  describe('When using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('button[type="submit"]');
+    });
+
+    test('form shows invalid inputs', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+});
+
+describe('When not logged in', async () => {
+  test('User cannot create blog post', async () => {
+    const result = await page.post('/api/blogs', { title: 'Not logged in', content: 'Haha you\'re not posted'});
+    expect(result.error).toEqual('You must log in!');
+  });
+
+  test('User cannot fetch blog list', async () => {
+    const result = await page.get('/api/blogs');
+    expect(result.error).toEqual('You must log in!');
+  });
+});
---
+++ tests/helpers/page.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/session-factory');
+const userFactory = require('../factories/user-factory');
+
+class Page {
+  static async build() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const customPage = new Page(page);
+
+    return new Proxy(customPage, {
+      get: (target, property) => {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  async login() {
+    const { session, sig } = sessionFactory(await userFactory());
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('localhost:3000/blogs');
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return await this.page.$eval(selector, el => el.innerHTML);
+  }
+
+  async get(path) {
+    return this.page.evaluate((_path) => {
+      return fetch(_path,{
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(data => data.json());
+    }, path);
+  }
+
+  async post(path, body) {
+    return this.page.evaluate((_path, _body) => {
+      return fetch(_path,{
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(_body)
+      }).then(data => data.json());
+    }, path, body);
+  }
+}
+
+module.exports = Page;
