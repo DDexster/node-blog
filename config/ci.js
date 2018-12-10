708a008835caf3264cc733af631c59d87948f0b3
Dmitry Bondarenko <bondarenko.dmitry.85@gmail.com>
2018-12-10 09:45:31 +0200

    travis config

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '368347247213-ouukl0o4j7m4ja2fptu9l8l9vlvpjevd.apps.googleusercontent.com',
+  googleClientSecret: 'h6QJDquYgp_29t6fNmUig3Zp',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisURL: 'redis://127.0.0.1:6379'
+};
