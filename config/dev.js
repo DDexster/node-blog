8f6a6efa79d38543a6a9a4a1881f1e0c3c7bf513
DDexster <d.bondrnko@gmail.com>
2019-02-19 09:58:42 +0200

    Delete dev.js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID: '368347247213-ouukl0o4j7m4ja2fptu9l8l9vlvpjevd.apps.googleusercontent.com',
-  googleClientSecret: 'h6QJDquYgp_29t6fNmUig3Zp',
-  mongoURI: 'mongodb://dev-user:123@ds247058.mlab.com:47058/node-blog-dev',
-  cookieKey: '123123123',
-  redisURL: 'redis://127.0.0.1:6379'
-};
